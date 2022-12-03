39807a334ac3b9e6183a6f165d318d88fe206b7e
Martin Mosler <git@martinmosler.de>
2022-12-03 20:25:13 +0100

    Day 3 part 2 solved

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,27 +1,35 @@
 use std::io;
 
 fn main() {
-    let rounds: u32 = io::stdin()
-        .lines()
-        .map(|line| {
-            let items = line.unwrap();
-            let item_count = items.len();
-            let (l, r) = items.split_at(item_count / 2);
-            common(l, r)
-        })
-        .sum();
-    println!("{:?}", rounds);
+    let mut lines = io::stdin().lines().map(|line| line.unwrap()).into_iter();
+    let mut priorities = Vec::<u32>::new();
+    loop {
+        let first = if let Some(n) = lines.next() { n } else { break };
+        let second = lines.next().unwrap();
+        let third = lines.next().unwrap();
+        let common_item = common(&common(&first, &second), &third)
+            .chars()
+            .next()
+            .unwrap();
+        priorities.push(priority(common_item));
+    }
+    println!("{:?}", priorities.iter().sum::<u32>());
 }
 
-fn common(l: &str, r: &str) -> u32 {
+fn common(l: &str, r: &str) -> String {
+    let mut common_items = String::new();
     for c in l.chars() {
         if r.contains(c) {
-            if c >= 'a' {
-                return c as u32 - 'a' as u32 + 1;
-            } else {
-                return c as u32 - 'A' as u32 + 27;
-            }
+            common_items.push(c);
         }
     }
-    0
+    common_items
+}
+
+fn priority(c: char) -> u32 {
+    if c >= 'a' {
+        c as u32 - 'a' as u32 + 1
+    } else {
+        c as u32 - 'A' as u32 + 27
+    }
 }
