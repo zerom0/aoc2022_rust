5e7edafcc02659bfdd6cede8d9b56f1f937b89a3
Martin Mosler <git@martinmosler.de>
2022-12-13 22:26:09 +0100

    Day 9 part 2 solved

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -28,41 +28,52 @@ impl Position {
     fn follow(&mut self, head_pos: &Position) {
         if head_pos.x - self.x == 2 {
             self.x += 1;
-            if head_pos.y != self.y {
-                self.y = head_pos.y;
+            if head_pos.y > self.y {
+                self.y += 1;
+            } else if head_pos.y < self.y {
+                self.y -= 1;
             }
         }
         if head_pos.x - self.x == -2 {
             self.x -= 1;
-            if head_pos.y != self.y {
-                self.y = head_pos.y;
+            if head_pos.y > self.y {
+                self.y += 1;
+            } else if head_pos.y < self.y {
+                self.y -= 1;
             }
         }
         if head_pos.y - self.y == 2 {
             self.y += 1;
-            if head_pos.x != self.x {
-                self.x = head_pos.x;
+            if head_pos.x > self.x {
+                self.x += 1;
+            } else if head_pos.x < self.x {
+                self.x -= 1;
             }
         }
         if head_pos.y - self.y == -2 {
             self.y -= 1;
-            if head_pos.x != self.x {
-                self.x = head_pos.x;
+            if head_pos.x > self.x {
+                self.x += 1;
+            } else if head_pos.x < self.x {
+                self.x -= 1;
             }
         }
     }
 }
 
-fn show(head: &Position, tail: &Position) {
-    (0..5).rev().for_each(|y| {
-        (0..5).for_each(|x| {
-            if head.x == x && head.y == y {
-                print!("H");
-            } else if tail.x == x && tail.y == y {
-                print!("T");
-            } else {
-                print!(".");
+fn show(knots: &Vec<Position>) {
+    (-5..16).rev().for_each(|y| {
+        (-11..15).for_each(|x| {
+            let mut marker = String::from(".");
+            if x == 0 && y == 0 {
+                marker = "s".to_string();
             }
+            for i in 0..knots.len() {
+                if knots[i].x == x && knots[i].y == y {
+                    marker = (knots.len() - i - 1).to_string();
+                }
+            }
+            print!("{}", marker);
         });
         println!();
     })
@@ -70,18 +81,23 @@ fn show(head: &Position, tail: &Position) {
 
 fn main() {
     let lines = std::io::stdin().lines().map(|line| line.unwrap());
-    let mut head = Position { x: 0, y: 0 };
-    let mut tail = Position { x: 0, y: 0 };
-    let mut tail_positions = vec![tail.clone()];
+    let mut knots = vec![Position { x: 0, y: 0 }];
+    (0..9).for_each(|_| knots.push(Position { x: 0, y: 0 }));
+    let mut tail_positions = vec![Position { x: 0, y: 0 }];
     for line in lines {
         let (direction, count) = line.split_once(' ').unwrap();
+        // repeat steps 'count' times
         for _ in 0..count.to_string().parse::<usize>().unwrap() {
-            head.move_one(direction);
-            tail.follow(&head);
-            tail_positions.push(tail.clone());
-            println!("{} Head {:?} Tail {:?}", direction, head, tail);
-            show(&head, &tail)
+            knots.last_mut().unwrap().move_one(direction);
+            for i in (0..(knots.len() - 1)).rev() {
+                // follow next knot
+                let last = knots[i + 1].clone();
+                knots[i].follow(&last);
+            }
+            tail_positions.push(knots[0].clone());
         }
+        println!("{} {}", count, direction);
+        show(&knots);
     }
     tail_positions.sort();
     tail_positions.dedup();
