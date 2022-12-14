cf164b759f38748593deeaf258717ece875e85c8
Martin Mosler <git@martinmosler.de>
2022-12-14 21:25:52 +0100

    Day 10 part 2 solved

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,21 +1,29 @@
 fn main() {
     let lines = std::io::stdin().lines().map(|line| line.unwrap());
-    let mut cycles = vec![1_i32];
+//    let mut cycles = vec![1_i32];
+    let mut current_cycle = 1_i32;
     let mut state = 1_i32;
     for line in lines {
         if line == "noop" {
-            cycles.push(state);
+            next_cycle(&mut current_cycle, &state);
         } else {
             let (_, amount) = line.split_once(' ').unwrap();
-            cycles.push(state);
-            cycles.push(state);
+            next_cycle(&mut current_cycle, &state);
+            next_cycle(&mut current_cycle, &state);
             state += amount.parse::<i32>().unwrap();
         }
     }
-    let mut sum = 0;
-    for i in [20, 60, 100, 140, 180, 220] {
-        println!("{} * {} = {}", i, cycles[i], i as i32 * cycles[i]);
-        sum += i as i32 * cycles[i];
+}
+
+fn next_cycle(cycle: &mut i32, state: &i32) {
+    if *cycle >= *state && *cycle <= (state + 2) {
+        print!("#");
+    } else {
+        print!(".");
     }
-    println!("Sum = {}", sum);
+    if *cycle % 40 == 0 {
+        println!();
+        *cycle = 0;
+    }
+    *cycle += 1;
 }
